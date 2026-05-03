ab0e633fbff41ace2180ae11fe2db763cae2a76b
sumithazard <49520525+sumithazard@users.noreply.github.com>
2026-05-03 21:23:44 +0530

    jumphost-ami-modification02

--- terraform_main_ec2/variables.tf
+++ terraform_main_ec2/variables.tf
@@ -64,7 +64,7 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-05cf1e9f73fbad2e2" // Replace with the latest AMI ID for your region
+  default     = "ami-0150ccaf51ab55a51" // Replace with the latest AMI ID for your region
 }
 
 variable "instance_type" {
