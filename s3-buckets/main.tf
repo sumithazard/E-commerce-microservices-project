50394f3e707621cc0beb6f104be334fb717638b3
sumithazard <49520525+sumithazard@users.noreply.github.com>
2026-05-08 13:37:58 +0530

    s3-bucket-code-modification

--- s3-buckets/main.tf
+++ s3-buckets/main.tf
@@ -5,6 +5,8 @@ provider "aws" {
 resource "aws_s3_bucket" "bucket1" {
   bucket = "sumit09ecommerce01"
 
+  force_destroy = true
+
   tags = {
     Name        = "sumit09ecommerce01"
     Environment = "dev"
@@ -20,6 +22,8 @@ resource "aws_s3_bucket_versioning" "bucket1_versioning" {
 
 resource "aws_s3_bucket" "bucket2" {
   bucket = "sumit09ecommerce02"
+
+  force_destroy = true
 
   tags = {
     Name        = "sumit09ecommerce02"
